7dc13b03804fe3ea45e5c35978734c93eadc207e
Gary Chen <chenhhgary@gmail.com>
2021-09-22 10:16:52 +0800

    init deployed_2

---
+++ Smart_Contract_Project/deployed_FISCO_BCOS/deployed_2/2_LguMetaverseBase.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+
+import "./1_ownable.sol";
+import "./safemath.sol";
+import "./erc721.sol";
+
+
+contract LguMetaverseBase is Ownable, ERC721 {
+    
+    using SafeMath for uint256;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
+    
+    event NewLguModel(uint LguModelId, string name, uint dna);
+    
+    //uint dnaDigits = 16;
+    //uint dnaModulus = 10 ** dnaDigits;
+    //uint cooldownTime = 1 days;
+    
+    struct LguModel {
+        string name;
+        uint dna;
+    }
+    
+    LguModel[] public LguModels;
+    
+    mapping (uint => address) public LguModelToOwner;
+    mapping (address => uint) ownerLguModelCount;
+    
+    
+    // This function should be "internal"
+    function CreateLguModel(string memory _name, uint _dna) public {
+        LguModels.push( LguModel(_name, _dna) );
+        uint id = LguModels.length - 1;
+        LguModelToOwner[id] = msg.sender;
+        ownerLguModelCount[msg.sender] = ownerLguModelCount[msg.sender].add(1);
+        emit NewLguModel(id, _name, _dna);
+    }
+    
+    // function _generateRandomDna(string memory _str) private view returns (uint);
+    // function createRandomZombie(string memory _name) public;
+    
+    
+    // modified from ZombieFeeding
+    modifier onlyOwnerOfModel(uint _modelId){
+        require(msg.sender == LguModelToOwner[_modelId]);
+        _;
+    }
+    
+    
+    // modified from ZombieHelper
+    function withdraw() external onlyOwner{
+        address payable _owner = payable(address(uint160(owner())));
+        _owner.transfer(address(this).balance);
+    }
+    
+    function changeName(uint _modelId, string calldata _newName) external onlyOwnerOfModel(_modelId) {
+        LguModels[_modelId].name = _newName;
+    }
+    
+    function getModelsByOwner(address _owner) external view returns(uint[] memory) {
+        uint[] memory result = new uint[](ownerLguModelCount[_owner]);
+        uint counter = 0;
+        for (uint i = 0; i < LguModels.length; i++) {
+            if (LguModelToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
+    
+    
+    // ERC721 implements
+    mapping (uint => address) modelApprovals;
+    
+    function balanceOf(address _owner) override external view returns (uint256) {
+        return ownerLguModelCount[_owner];
+    }
+    
+    function ownerOf(uint256 _tokenId) override external view returns (address) {
+        return LguModelToOwner[_tokenId];
+    }
+    
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownerLguModelCount[_to] = ownerLguModelCount[_to].add(1);
+        ownerLguModelCount[msg.sender] = ownerLguModelCount[msg.sender].sub(1);
+        LguModelToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _tokenId) override external payable {
+        require ( LguModelToOwner[_tokenId] == msg.sender || modelApprovals[_tokenId] == msg.sender );
+        _transfer(_from, _to, _tokenId);
+    }
+    
+    function approve(address _approved, uint256 _tokenId) override external payable onlyOwnerOfModel(_tokenId) {
+        modelApprovals[_tokenId] = _approved;
+        emit Approval(msg.sender, _approved, _tokenId);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Smart_Contract_Project/deployed_FISCO_BCOS/deployed_2/README.md
@@ -0,0 +1,14 @@
+# XXX.sol
+
+## 1. Info
+
+* Contract Address: XXXXXXXXXXXX
+* Owner: XXXXXXXXXXXX
+
+## 2. Process
+
+* Compile in Remix IDE (get Bytecode and ABI) using compiler version **0.4.25**
+
+* Deployed in WeBASE
+
+  
