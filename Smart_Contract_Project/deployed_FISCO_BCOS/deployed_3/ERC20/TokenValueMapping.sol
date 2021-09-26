e4e13babea4e60d066c486cb65e3effca1c85f1b
Gary Chen <chenhhgary@gmail.com>
2021-09-26 23:09:55 +0800

    implement basic structure of deployed_3/ERC20

---
+++ Smart_Contract_Project/deployed_FISCO_BCOS/deployed_3/ERC20/TokenValueMapping.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity>=0.4.24 <0.6.11;
+
+import "./ownable.sol";
+
+contract TokenValueMapping is Ownable {
+    mapping (string => uint256) internal _tokenValueMapping;
+    string[] private LocationSet;
+    
+    constructor() internal {
+        // init
+        _tokenValueMapping ["TA"] = 1;
+        _tokenValueMapping ["TB"] = 1;
+        _tokenValueMapping ["TC"] = 1;
+        _tokenValueMapping ["TD"] = 1;
+        _tokenValueMapping ["University Library"] = 1;
+        _tokenValueMapping ["Harmonia"] = 1;
+        _tokenValueMapping ["Muse"] = 1;
+        _tokenValueMapping ["Diligentia"] = 1;
+        _tokenValueMapping ["Shaw"] = 1;
+        
+        LocationSet.push("TA");
+        LocationSet.push("TB");
+        LocationSet.push("TC");
+        LocationSet.push("TD");
+        LocationSet.push("University Library");
+        LocationSet.push("Harmonia");
+        LocationSet.push("Muse");
+        LocationSet.push("Diligentia");
+        LocationSet.push("Shaw");
+    }
+    
+    modifier isKeyValid(string key) {         // is this modifier works??? NO...
+        for (uint i = 0; i < LocationSet.length; i++) {
+            if (keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked( LocationSet[i] ))) {
+                _;
+            }
+        }
+    }
+    
+    modifier isKeyInvalid(string key) {         // is this modifier works??? NO...
+        for (uint i = 0; i < LocationSet.length; i++) {
+            if (keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked( LocationSet[i] ))) {
+                require(false);
+            }
+        }
+        _;
+    }
+    
+    function SetTokenValueMapping(string key, uint256 value) external isKeyValid(key) onlyOwner {
+        _tokenValueMapping[key] = value;
+    }
+    
+    function AddLocation(string key, uint256 value) external isKeyInvalid(key) onlyOwner {
+        _tokenValueMapping [key] = value;
+    }
+}
--- Smart_Contract_Project/deployed_FISCO_BCOS/deployed_3/ERC20/erc20.sol
+++ Smart_Contract_Project/deployed_FISCO_BCOS/deployed_3/ERC20/erc20.sol
@@ -5,14 +5,13 @@ interface ERC20 {
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    function name() public view returns (string);
-    function symbol() public view returns (string);
-    function decimals() public view returns (uint8);
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address _owner) public view returns (uint256 balance);
-    function transfer(address _to, uint256 _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-    function approve(address _spender, uint256 _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
-
+    function name() external view returns (string);
+    function symbol() external view returns (string);
+    //function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 }
