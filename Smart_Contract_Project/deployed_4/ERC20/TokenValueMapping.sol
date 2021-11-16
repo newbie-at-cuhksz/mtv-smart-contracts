6619f1ab60c3d72b9ff949234abbf9d6aaca648c
Gary Chen <chenhhgary@gmail.com>
2021-11-16 21:37:50 +0800

    Update TokenValueMapping.sol

--- Smart_Contract_Project/deployed_4/ERC20/TokenValueMapping.sol
+++ Smart_Contract_Project/deployed_4/ERC20/TokenValueMapping.sol
@@ -4,54 +4,36 @@ pragma solidity>=0.4.24 <0.6.11;
 import "./LguTokenERC20Base.sol";
 
 contract TokenValueMapping is LguTokenERC20Base {
-    mapping (string => uint256) internal _tokenValueMapping;
-    string[] private LocationSet;
+    
+    struct RegionInfo {
+        string name;
+        uint256 base;
+        uint256 weight;
+    }
+    
+    uint256 totalBonus = 200;
+    
+    RegionInfo[] private _regions;
+
     
     constructor() internal {
-        // init
-        _tokenValueMapping ["TA"] = 1;
-        _tokenValueMapping ["TB"] = 1;
-        _tokenValueMapping ["TC"] = 1;
-        _tokenValueMapping ["TD"] = 1;
-        _tokenValueMapping ["University Library"] = 1;
-        _tokenValueMapping ["Harmonia"] = 1;
-        _tokenValueMapping ["Muse"] = 1;
-        _tokenValueMapping ["Diligentia"] = 1;
-        _tokenValueMapping ["Shaw"] = 1;
+        _regions.push( RegionInfo("University Library", 20, 100) );
+        _regions.push( RegionInfo("TA", 15, 100) );
+        _regions.push( RegionInfo("Shaw", 10, 100) );
+        _regions.push( RegionInfo("Gym", 20, 100) );
+    }
+    
+    modifier isRegionNameValid(string regionName) {         // is this modifier works??? 
+        bool tmp = false;
+        for (uint i = 0; i < _regions.length; i++) {
+            if (keccak256(abi.encodePacked(regionName)) == keccak256(abi.encodePacked( _regions[i].name ))) {
+                tmp = true;
+                break;
+            }
+        }
         
-        LocationSet.push("TA");
-        LocationSet.push("TB");
-        LocationSet.push("TC");
-        LocationSet.push("TD");
-        LocationSet.push("University Library");
-        LocationSet.push("Harmonia");
-        LocationSet.push("Muse");
-        LocationSet.push("Diligentia");
-        LocationSet.push("Shaw");
-    }
-    
-    modifier isKeyValid(string key) {         // is this modifier works??? NO...
-        for (uint i = 0; i < LocationSet.length; i++) {
-            if (keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked( LocationSet[i] ))) {
-                _;
-            }
-        }
-    }
-    
-    modifier isKeyInvalid(string key) {         // is this modifier works??? NO...
-        for (uint i = 0; i < LocationSet.length; i++) {
-            if (keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked( LocationSet[i] ))) {
-                require(false);
-            }
-        }
+        require(tmp);
         _;
     }
-    
-    function SetTokenValueMapping(string key, uint256 value) external isKeyValid(key) onlyOwner {
-        _tokenValueMapping[key] = value;
-    }
-    
-    function AddLocation(string key, uint256 value) external isKeyInvalid(key) onlyOwner {
-        _tokenValueMapping [key] = value;
-    }
+
 }
